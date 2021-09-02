6f4bb4ba6e525e6388882c86853c342e861cf863
cmsong111 <cmsong111@naver.com>
2021-09-02 16:53:32 +0900

    add

---
+++ CodeUP/Python basic 100/6064.py
@@ -0,0 +1,4 @@
+a,b,c = map(int, input().split())
+
+c=[a,b,c]
+print(min(c))
---
+++ CodeUP/Python basic 100/6066.py
@@ -0,0 +1,9 @@
+def oe(z):
+    if z%2==0 :
+      print("even")
+    else :
+      print("odd")
+a,b,c = map(int, input().split())
+oe(a)
+oe(b)
+oe(c)
