12741fb0540d2ef4926257aa04501ea1536cd26d
Kimnamju <cmsong111@naver.com>
2021-10-02 23:44:53 +0900

    C# start

---
+++ CSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace BrainCSharp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Writeline("사용법 : Helloworld.exe <이름>");
+                return;
+            }
+        
+            Writeline("Hello, {0}!", args[0]);
+        }
+    }
+}
