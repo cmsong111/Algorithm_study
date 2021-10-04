79df4962a122f87d4e6cd529ad31cf2913c0f3b5
cmsong111 <cmsong111@naver.com>
2021-10-04 16:26:48 +0900

    solved probleam

---
+++ BAEKJOON/Python/10818.py
@@ -0,0 +1,5 @@
+a = int(input())
+lis=[]
+temp = list(map(int,input().split()))
+
+print(min(temp),max(temp))
--- BAEKJOON/Python/1110.py
+++ BAEKJOON/Python/1110.py
@@ -9,24 +9,27 @@ while(True):
         print("1")
         break
 
+    
     #합 구하기
     if temp < 10:
         sum = temp
     elif temp >= 10:
         sum = temp//10 + temp%10
 
-    #같으면 함수 종료
-    if a == sum:
-        print(count)
-        break
-
-     #카운트 증가
-    count = count + 1
      #숫자 변경
     if temp < 10:
         temp = temp*10 + temp
     elif temp >= 10:
         temp = ((temp%10)*10 + (sum%10)) 
+
+
+    #같으면 함수 종료
+    if a == temp:
+        print(count)
+        break
+
+     #카운트 증가
+    count = count + 1
 
 
     if count > 1000000:
