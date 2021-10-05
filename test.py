ca8c995d2f77cff98053f5f41b7c14a72c77631c
Kimnamju <cmsong111@naver.com>
2021-10-05 22:08:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,18 +2,19 @@ class JSS:
     def __init__(self):
         self.name = input("이름: ")
         self.age = input("나이: ")
-    
+
+class JSS2(JSS):
+    def __init__(self):
+        super().__init__()
+        self.sex = input("성별: ")
+
     def show(self):
         print("나의 이름은 {}. 나이는 {}세 입니다.".format(self.name, self.age))
+        print("성별은 {} 입니다.".format(self.sex))
 
-a = JSS()
-a.name
-a.age
-a.show()
 
-b = JSS()
-b.name
-b.age
-b.show()
 
-print(b.age)
+a = JSS2()
+
+print(a.sex)
+print(a.age)
