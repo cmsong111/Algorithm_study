9f28f374779d19f7f035ae7524190d262a17c6e4
Kimnamju <cmsong111@naver.com>
2021-10-07 11:08:54 +0900

    add

---
+++ BAEKJOON/Python/2754.py
@@ -0,0 +1,32 @@
+a = input()
+#A시리즈
+if a =="A+":
+    print(4.3)
+if a =="A0":
+    print(4.0)
+if a =="A-":
+    print(3.7)
+#B시리즈
+if a =="B+":
+    print(3.3)
+if a =="B0":
+    print(3.0)
+if a =="B-":
+    print(2.7)
+#C시리즈
+if a =="C+":
+    print(2.3)
+if a =="C0":
+    print(2.0)
+if a =="C-":
+    print(1.7)
+#D시리즈
+if a =="D+":
+    print(1.3)
+if a =="D0":
+    print(1.0)
+if a =="D-":
+    print(0.7)
+#F????
+if a =="F":
+    print("0.0")
