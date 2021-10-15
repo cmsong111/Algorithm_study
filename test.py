fe97bc3ebd010558d22700b293f7db04f337ee84
cmsong111 <cmsong111@naver.com>
2021-10-15 11:57:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-def hello(x,b):
-    return (x+b)
+a = list(input().split("-"))
+for i in range(len(a)):
+    b = []
+    b.append(a[i].split("+"))
+    temp = 0
+    for i1 in range(len(b)):
+        for i2 in range(len(b[i1])):
+            tmp = b[i1][i2]
+            temp = temp + int(tmp)
+    a[i] = int(temp)
 
-x = 10
-y = 20
-hello(x,y)
+sum = int(a[0])
+for i in range(1,len(a),1):
+    sum -=int(a[i])
+print(sum)
