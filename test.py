9b88b65321c85c9bf6bca77a3025d955a732e1fe
Kimnamju <cmsong111@naver.com>
2021-10-15 19:00:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,40 @@
-def hello(x,b):
-    return (x+b)
+num = int(input())
+count = 0
+while(1):
+    if num == 1:
+        break
+    
+    if num % 3 == 1:
+        num -=1
+        count +=1
+        print(num)
+        num /=3
+        count +=1
+        print(num)
 
-x = 10
-y = 20
-hello(x,y)
+
+    elif num % 3 == 0:
+        num = num / 3
+        count +=1
+        print(num)
+
+    elif num % 2 == 1:
+        num = num -1
+        count +=1
+        print(num)
+        num = num / 2
+        count +=1
+        print(num)
+
+    elif num % 2 == 0:
+        num = num / 2
+        count +=1
+        print(num)
+
+    else:
+        num = num - 1
+        count +=1
+        print(num)
+    
+    
+print(count)
