bba2f44e4fad7cc145eaa038d010d6e7d480e8a4
cmsong111 <cmsong111@naver.com>
2021-10-19 22:12:20 +0900

    Create: solved problem

--- CodeUP/Python basic 100/1002.py
+++ CodeUP/Python basic 100/1002.py
@@ -9,10 +9,8 @@ for i in range(num):
     x1,y1,r1,x2,y2,r2 = map(int, sys.stdin.readline().split())
    
     #원이 일치 할때
-    if x1 == x2:
-        if y1 == y2:
-            if r1==r2:
-                print("-1")
+    if x1 == x2 and y1 == y2 and r1==r2:
+        print("-1")
     #점이 하나일때
     elif ((x2-x1)**2+(y2-y1)**2)==(r1+r2)**2:
         print("1")
