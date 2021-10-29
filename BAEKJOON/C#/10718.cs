bcfe293e817e124c78bb15351a77ab5a56c70f07
Kimnamju <cmsong111@naver.com>
2021-10-29 14:23:01 +0900

    Create:solved problem

---
+++ BAEKJOON/C#/10718.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharp_study
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강한친구 대한육군");
+            Console.WriteLine("강한친구 대한육군");
+        }
+    }
+}
