91462041553c772ff65816be3f0c3ede2da15b72
cmsong111 <cmsong111@naver.com>
2021-11-17 19:38:23 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-
 import sys
 
-M = int(sys.stdin.readline())
-ML = list(sys.stdin.readline().split())
-N = int(sys.stdin.readline())
-NL = list(sys.stdin.readline().split())
+a = int(sys.stdin.readline())
+result = 0
 
-ML.sort()
+while(a>=5):
+    a -= 5
+    result +=1
 
-print(ML)
+while(a>=3):
+    a -= 3
+    result +=1
+
+if a==0:
+    print(result)
+else:
+    print("-1")
+
+
+
+
