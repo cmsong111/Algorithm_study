f5a0cef91db0fd8672d784569aadcfabcc4c8b89
Kimnamju <cmsong111@naver.com>
2021-12-03 19:12:21 +0900

    Create 23804.py

---
+++ BAEKJOON/Python/23804.py
@@ -0,0 +1,17 @@
+a = int(input())
+
+for i in range(a):
+    for ii in range(a*5):
+        print("@",end="")
+    print()
+
+for i in range(a*3):
+    for ii in range(a):
+        print("@",end="")
+    print()
+
+    
+for i in range(a):
+    for ii in range(a*5):
+        print("@",end="")
+    print()
