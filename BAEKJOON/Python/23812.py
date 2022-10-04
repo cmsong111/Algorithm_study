1b249942a2443ffb2404d84cfc7df2fddb856536
Namju Kim <cmsong111@naver.com>
2022-10-05 00:54:03 +0900

    Add files via upload

---
+++ BAEKJOON/Python/23812.py
@@ -0,0 +1,25 @@
+count = int(input())
+
+for i in range(count):
+    print("@"*count,end="")
+    for ii in range (count):
+        print(" "*3,end="")
+    print("@"*count)
+
+for i in range(count):
+    print("@"*(count*5))
+
+for i in range(count):
+    print("@"*count,end="")
+    for ii in range (count):
+        print(" "*3,end="")
+    print("@"*count)
+
+for i in range(count):
+    print("@"*(count*5))
+
+for i in range(count):
+    print("@"*count,end="")
+    for ii in range (count):
+        print(" "*3,end="")
+    print("@"*count)
